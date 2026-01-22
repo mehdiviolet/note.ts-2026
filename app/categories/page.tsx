a9a4af359a69cdfe529e5fed85ff8bc31ddb1803
mehdiviolet <mehdiviolet@yahoo.com>
2026-01-22 17:41:26 +0100

    mockup pronto per praticare prisma - devo creare actions, types ecc. per ora aabbiamo solo mockup

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,7 +1,7 @@
 export default async function CategoriesPage() {
   return (
     <div className="max-w-5xl mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">Categorie</h1>
+      <h1 className="text-3xl font-bold mb-8">Categorie.</h1>
 
       <div className="grid md:grid-cols-2 gap-8">
         {/* Form */}
